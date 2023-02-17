8f513149ee94a20ff5b919669957dfd792189f23
LesleyNthane <nthanelesley2001@gmail.com>
2023-02-17 06:48:00 -0800

    Task 5 0f C variables, if statements and loops project

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main will output single digits from 0 to 9
+ *main function returns 0
+ */
+int main(void)
+{
+int number;
+
+for (number = 0; number < 10; number++)
+{
+printf("%d", number);
+}
+printf("\n");
+return (0);
+}
