75d93aa835f62e0d2b38a49c8a994dbec52b2fff
LesleyNthane <nthanelesley2001@gmail.com>
2023-02-17 03:50:47 -0800

    Task 0 0f C variables, if statements and loops project

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ *main function will output if the number is positive, negative or zero
+ *the main function returns 0
+ */
 int main(void)
 {
 int n;
