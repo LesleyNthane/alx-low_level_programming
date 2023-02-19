7a2c124681de45634e39e4ba4c4b5af9e40de9d9
LesleyNthane <nthanelesley2001@gmail.com>
2023-02-19 09:39:37 -0800

    Task 9 c variables if else while

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /**
- *main function output all combination of single digits
- *main function returns o
- */
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
 int main(void)
 {
 int number;
