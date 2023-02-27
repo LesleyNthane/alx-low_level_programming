a549f4496eb5ca74451bea88a29c9763e3c7dd51
LesleyNthane <nthanelesley2001@gmail.com>
2023-02-27 05:57:08 -0800

    Task 1 of C pointers array strings

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * swap_int - swap integer a and b
+ * @a: first integer to be swapped to second integer
+ * @b: second integer to be swapped to first integer
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+int x = 0;
+
+*a = x;
+*a = *b;
+*b = x;
+}
